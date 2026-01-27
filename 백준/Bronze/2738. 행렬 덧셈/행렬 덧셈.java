320cd2ee3d1b5d05ff831acc0fe4074db9bb2409
Beomsoo Park <linoleca@naver.com>
2026-01-27 22:32:39 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 136 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] matrix = new int[N][M];
+
+        // 행렬 A 저장
+        for(int i = 0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<M; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 행렬 B 저장하며 더하기
+        for(int i = 0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<M; j++) {
+                sb.append(matrix[i][j] += Integer.parseInt(st.nextToken())).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
