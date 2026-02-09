06215682a692e1807ea3a66ca31485c3af47baa7
Beomsoo Park <linoleca@naver.com>
2026-02-09 18:17:03 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        int[] arr = new int[31];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 28; i++) {
+            arr[Integer.parseInt(br.readLine())] = 1;
+        }
+
+        for (int i = 1; i <= 30; i++) {
+            if(arr[i] != 1) System.out.println(i);
+        }
+    }
+}
